86aac0f0d64596ed869c440d044c7dd87c89c926
egxn <afvelosas@gmail.com>
2026-08-05 23:14:04 -0500

    Clean up chess code generator

--- print_labo/src/print_labo/misc_prints/08_chess.py
+++ print_labo/src/print_labo/misc_prints/08_chess.py
@@ -1,193 +1,159 @@
+"""Mechanical parts for the chess-board mechanism.
+
+Dimensions are in millimetres.  The vocabulary intentionally follows common
+mechanical-design terms: housing, shaft, keyway, mounting flange, bore and
+clearance.
+"""
+
 from tanuki.dsl import *
 
-CLR = 0.125  # mm
+
+CLEARANCE = 0.125
 
 
-def new_motor(clr=0):
-    motor = cylinder(28/2 + clr, 19 + clr, "motor") | place(0, 0, 0)
-    axis_1 = cylinder(9/2, 1.5, "axis_1") | place(0, 8, 19/2 + 1.5/2)
-    axis_2 = cylinder(5/2, 10, "axis_2") | place(0, 8, 19/2 + 10/2)
-    axis_3 = cube(10, 3, 10, "axis_3") | place(0, 8, 19/2 + 10/2)
+def build_motor_assembly(clearance: float = 0.0):
+    """Build only the motor body and drive components.
 
-    axis_3 = intersect([axis_3, axis_2])
-    axis_2 = difference(axis_2, [axis_3])
-    axis_2_5 = cylinder(5/2, 4, "axis_2_5") | place(0, 8, 19/2 + 2)
-    wire_box = cube(14.6, 5, 19 + clr, "wire_box") | place(0, -17 + 5/2, 0)
+    The mounting flange belongs to the chess-board base, not to the motor
+    assembly itself.
+    """
+    housing_diameter = 28 + 2 * clearance
+    housing_height = 19 + clearance
+    housing = cylinder(housing_diameter / 2, housing_height, "motor_housing")
 
-    bolt_support_1 = cube(
-        35, 7, 2 + clr, "bolt_support") | place(0, 0, 19/2 - 2/2)
-    bolt_support_2 = cylinder(
-        7/2, 2 + clr, "bolt_support") | place(35/2, 0, 19/2 - 2/2)
-    bolt_support_3 = cylinder(
-        7/2, 2 + clr, "bolt_support") | place(-35/2, 0, 19/2 - 2/2)
-    h_bolt_support_1 = cylinder(
-        4/2, 2 + clr, "bolt_support") | place(35/2, 0, 19/2 - 2/2)
-    h_bolt_support_2 = cylinder(
-        4/2, 2 + clr, "bolt_support") | place(-35/2, 0, 19/2 - 2/2)
-    bolt_support = union([bolt_support_1, bolt_support_2, bolt_support_3])
-    bolt_support = difference(
-        bolt_support, [h_bolt_support_1, h_bolt_support_2])
+    output_shaft = cylinder(9 / 2, 1.5, "output_shaft") | place(0, 8, 19 / 2 + 1.5 / 2)
+    drive_shaft = cylinder(5 / 2, 10, "drive_shaft") | place(0, 8, 19 / 2 + 10 / 2)
+    keyway_tool = cube(10, 3, 10, "shaft_keyway_tool") | place(0, 8, 19 / 2 + 10 / 2)
+    keyway_tool = intersect([keyway_tool, drive_shaft])
+    drive_shaft = difference(drive_shaft, [keyway_tool])
+    shaft_collar = cylinder(5 / 2, 4, "shaft_collar") | place(0, 8, 19 / 2 + 2)
 
-    motor = union([motor, axis_1, axis_2_5,  axis_3, wire_box, bolt_support])
-
-    return motor, bolt_support
+    cable_pocket = cube(14.6, 5, housing_height, "cable_pocket") | place(0, -14.5, 0)
+    motor = union([
+        housing,
+        output_shaft,
+        shaft_collar,
+        keyway_tool,
+        cable_pocket,
+    ])
+    return motor
 
 
-def create_motor():
-    with model("motor") as ctx:
-        motor, _ = new_motor()
-        output(motor)
-
-    return ctx.graph
-
-
-def create_motor_base_1():
-    with model("motor_base_1") as ctx:
-        motor, bolt_support = new_motor(CLR)
-        base_1 = cylinder(31/2, 19, "base")
-        base_2 = cube(17, 10, 19, "base_2") | place(0, -17 + 7/2, 0)
-
-        base = union([
-            base_1,
-            base_2,
-            bolt_support | place(0, 0, - 2.125),
-            bolt_support | place(0, 0, - 19 + 2.0625)
-        ])
-
-        h_wires = cube(12, 10, 5, "h_wires") | place(0, -20, 7)
-
-        base = difference(base, [motor, h_wires])
-        output(base)
-
-    return ctx.graph
+def build_mounting_flange(clearance: float = 0.0, z: float = 8.5):
+    """Build the transverse flange used to retain the motor."""
+    flange_thickness = 2 + clearance
+    flange = cube(35, 7, flange_thickness, "mounting_flange") | place(0, 0, z)
+    left_boss = cylinder(7 / 2, flange_thickness, "left_mounting_boss") | place(-17.5, 0, z)
+    right_boss = cylinder(7 / 2, flange_thickness, "right_mounting_boss") | place(17.5, 0, z)
+    left_bore = cylinder(4 / 2, flange_thickness, "left_mounting_bore") | place(-17.5, 0, z)
+    right_bore = cylinder(4 / 2, flange_thickness, "right_mounting_bore") | place(17.5, 0, z)
+    flange = union([flange, left_boss, right_boss])
+    return difference(flange, [left_bore, right_bore])
 
 
-def create_motor_base_2():
-    with model("motor_base_2") as ctx:
-        motor, bolt_support = new_motor(CLR)
-        base_1 = cylinder(31/2, 19, "base")
-        base_2 = cube(17, 10, 19, "base_2") | place(0, -17 + 7/2, 0)
+def build_motor_mount(clearance: float = CLEARANCE):
+    """Build the cylindrical motor seat with its rear cable relief."""
+    motor = build_motor_assembly(clearance)
+    mounting_flange = build_mounting_flange(clearance, 19 / 2 - 1)
+    motor_flange = build_mounting_flange(clearance, 19 / 2 + 1)
+    seat = cylinder(31 / 2, 19, "motor_seat")
+    rear_bracket = cube(17, 10, 19, "rear_bracket") | place(0, -13.5, 0)
+    cable_relief = cube(12, 10, 5, "cable_relief") | place(0, -20, 7)
+    mount = union([seat, rear_bracket, mounting_flange | place(0, 0, -2.125)])
+    return difference(mount, [motor, cable_relief, motor_flange | place(0, 0, -2)])
 
-        base = union([
-            base_1,
-            base_2,
-            bolt_support | place(0, 0, - 2.125),
-        ])
 
-        h_wires = cube(12, 10, 3, "h_wires") | place(0, -20, 8)
+def build_drive_coupler(clearance: float = CLEARANCE, ball_joint: bool = False):
+    """Build the shaft coupler, optionally with its ball-joint support."""
+    shaft = cylinder(6 / 2, 11, "coupler_shaft")
+    keyway = cylinder(5 / 2 + clearance, 6 + clearance, "coupler_keyway")
+    keyway_tool = cube(10 + clearance, 3, 6 + clearance, "coupler_keyway_tool")
+    keyway = intersect([keyway, keyway_tool]) | place(0, 0, -2.5)
+    shaft = difference(shaft, [keyway])
+
+    cross_boss = cylinder(7, 10, "cross_boss") | rotate(90, 0, 0) | place(0, 8, -1.5)
+    cross_bore = cylinder(9 / 2 + clearance, 55 + clearance, "cross_bore") | rotate(90, 0, 0) | place(0, 32, -1.5)
+    coupler = difference(union([shaft, cross_boss]), [cross_bore])
+    retaining_groove = cylinder(11 / 2, 2, "retaining_groove") | place(0, 0, -8)
+    coupler = difference(coupler, [retaining_groove])
+    mounting_tab = cube(10, 16, 2.5, "mounting_tab") | place(0, 5, 4.5)
+    coupler = union([coupler, mounting_tab])
+
+    if ball_joint:
+        ball = sphere(2.5 + 2 * clearance, "ball") | place(0, 0, 3.5)
+        coupler = difference(coupler, [ball])
+    return coupler
+
+
+def make_graph(name, geometry):
+    with model(name) as context:
+        output(geometry)
+    return context.graph
+
+
+def create_motor_assembly_graph():
+    motor = build_motor_assembly()
+    return make_graph("motor_assembly", motor)
+
+
+def create_motor_mount_plate_graph():
+    mount = build_motor_mount()
+    plate_mount_flange = build_mounting_flange(z=8.5)
+    mount = union([mount, plate_mount_flange | place(0, 0, -17)])
+    housing = cylinder(28 / 2, 20, "motor_seat")
+
+    return make_graph("motor_mount_plate", difference(mount, [housing]))
+
+
+def create_motor_mount_with_arm_graph():
+    mount = build_motor_mount()
+    cable_relief = cube(12, 10, 3, "cable_relief") | place(0, -20, 8)
+    arm = cube(17, 10, 16, "arm_support")
+    arm_bore = cylinder(9 / 2, 11, "arm_bore") | rotate(90, 0, 0) | place(0, -2.5, -1)
+    arm = difference(arm, [arm_bore]) | place(0, -23.5, -1.5)
+    return make_graph("motor_mount_with_arm", union([difference(mount, [cable_relief]), arm]) | rotate(0, 180, 0) | place(0, 88, 15))
+
+
+def create_reversed_motor_assembly_graph():
+    motor = build_motor_assembly()
+    return make_graph("reversed_motor_assembly", motor | rotate(0, 180, 0) | place(0, 88, 15))
+
+
+def create_shaft_coupler_graph(with_ball_joint: bool = False, name: str = "shaft_coupler", y: float = 19):
+    coupler = build_drive_coupler(ball_joint=with_ball_joint)
+    rotate_y = 180 if with_ball_joint else 0
+    place_y = 96 if with_ball_joint else y - 11
+    place_z = -4 if with_ball_joint else y 
     
-        arm_support = cube(17, 10, 16, "arm_support")
-        h_arm_support = cylinder(9/2, 11, "h_arm_support") | rotate(90, 0, 0) | place(0, -2.5, -1)
-        arm_support = difference(arm_support, [h_arm_support]) | place(0, -23.5, -1.5)
-
-        base = difference(base, [motor, h_wires])
-        base = union([base, arm_support]) | rotate(0, 180, 0) | place(0, 88, 15)
-
-        output(base)
-
-    return ctx.graph
+    return make_graph(name, coupler | rotate(0, rotate_y, 0) | place(0, place_y, place_z))
 
 
-def create_motor_2():
-    with model("motor_2") as ctx:
-        motor, _ = new_motor()
-        motor = motor | rotate(0, 180, 0) | place(0, 88, 15)
-        output(motor)
+def create_linkage_rod_graph(name: str = "linkage_rod", y: float = 40, z: float = 17.5):
+    leg = cylinder(9 / 2, 55, "linkage_rod") | rotate(90, 0, 0) | place(0, y, z)
+    return make_graph(name, leg)
 
-    return ctx.graph
-
-def create_motor_tap():
-    with model("motor_tap") as ctx:
-        axis_2 = cylinder(5/2 + CLR, 6 + CLR, "axis_2")
-        axis_3 = cube(10 + CLR, 3, 6 + CLR, "axis_3")
-        h_axis = intersect([axis_3, axis_2]) | place(0, 0, -2.5)
-        axis = cylinder(6/2, 11, "axis") | place(0, 0, 0)
-        axis = difference(axis, [h_axis])
-
-        tap_3 = cylinder(7, 10, "tap") | rotate(90, 0, 0) | place(0, 8, -1.5)
-        h_leg = cylinder(9/2 + CLR, 55 + CLR, "leg") | rotate(90, 0, 0) | place(0, 32, -1.5)        
-        tap_3 = difference(tap_3, [h_leg])
-        
-        tap = union([axis, tap_3])
-        h_base_m = cylinder(11/2, 2, "base") | place(0, 0, -8)
-        tap = difference(tap, [h_base_m]) 
-        
-        base = cube(10, 16, 2.5, "base") | place(0, 5, 4.5)
-        tap = union( [tap, base]) | place(0, 8, 19)
-        output(tap)
-
-    return ctx.graph
-
-def create_leg():
-    with model("leg") as ctx:
-        leg = cylinder(9/2, 55, "leg") | rotate(90, 0, 0) | place(0, 40, 17.5)
-        output(leg)
-        
-    return ctx.graph
-
-def create_motor_tap_2():
-    with model("motor_tap_2") as ctx:
-        axis_2 = cylinder(5/2 + CLR, 6 + CLR, "axis_2")
-        axis_3 = cube(10 + CLR, 3, 6 + CLR, "axis_3")
-        h_axis = intersect([axis_3, axis_2]) | place(0, 0, -2.5)
-        axis = cylinder(6/2, 11, "axis") | place(0, 0, 0)
-        axis = difference(axis, [h_axis])
-
-        tap_3 = cylinder(7, 10, "tap") | rotate(90, 0, 0) | place(0, 8, -1.5)
-        h_leg = cylinder(9/2 + CLR, 55 + CLR, "leg") | rotate(90, 0, 0) | place(0, 32, -1.5)        
-        tap_3 = difference(tap_3, [h_leg])
-        
-        tap = union([axis, tap_3])
-        h_base_m = cylinder(11/2, 2, "base") | place(0, 0, -8)
-        tap = difference(tap, [h_base_m]) 
-        
-        base = cube(10, 16, 2.5, "base") | place(0, 5, 4.5)
-
-        crystall_ball = sphere(5 + CLR*2, "crystall_ball") | place(0, -10, 1)
-        h_ring = cylinder(8, 3, "ring") | place(0, -10, 1)
-        ball_input = cube(6, 6, 10, "ball_input") | place(0, -15, 1)
-        ball_support = difference(h_ring, [ball_input ,crystall_ball])
-        base_ball_support = cube(10, 2, 6.25, "base") | place(0, -3, 2.625)
-        support = union([ball_support, base_ball_support])
-
-        crystall_ball = sphere(5, "crystall_ball") | place(0, -10, 1)
-
-        tap = union( [tap, base, ball_support, support]) | rotate(0, 180, 0) | place(0, 96, -4)
-
-        output(tap)
-
-    return ctx.graph
-
-def create_leg_2():
-    with model("leg_2") as ctx:
-        leg = cylinder(9/2, 55, "leg") | rotate(90, 0, 0) | place(0, 128, -2.5)
-        output(leg)
-        
-    return ctx.graph
 
 ALL_PARTS = [
-    create_motor, 
-    create_motor_base_1(),
-    create_motor_2,
-    create_motor_base_2(),
-    create_motor_tap(),
-    create_leg(),
-    create_motor_tap(),
-    create_motor_tap_2(),
-    create_leg_2(),
+    create_motor_assembly_graph(),
+    create_motor_mount_plate_graph(),
+    create_reversed_motor_assembly_graph(),
+    create_motor_mount_with_arm_graph(),
+    create_shaft_coupler_graph(),
+    create_linkage_rod_graph(),
+    create_shaft_coupler_graph(with_ball_joint=True, name="ball_joint_coupler"),
+    create_linkage_rod_graph(name="rear_linkage_rod", y=128, z=-2.5),
 ]
 
 
 if __name__ == "__main__":
     from pathlib import Path
-
     from print_labo.utils.compile_cli import run_compile_cli
 
     run_compile_cli(
         graphs=ALL_PARTS,
-        description="Compile zebra board parts",
+        description="Compile chess-board mechanical parts",
         source_script=Path(__file__).resolve(),
-        default_output="zebra.py",
-        default_output_dir="zebra_parts",
+        default_output="chess.py",
+        default_output_dir="chess_parts",
         watch_base_dir=Path(__file__).resolve().parent,
     )
